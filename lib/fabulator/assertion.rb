78c79d09baa5919672efae5bff13e8a0c43a1318
James Smith <jgsmith@tamu.edu>
2010-01-21 22:39:15 +0000

    Add support for model base urls, allow information to be overwritten or merged (default is overwritten)

--- lib/fabulator/assertion.rb
+++ lib/fabulator/assertion.rb
@@ -7,6 +7,7 @@ module Fabulator
       # f:select ...
       xsm_exp_parser = Fabulator::XSM::ExpressionParser.new
       @from  = xsm_exp_parser.parse((xml.attributes.get_attribute_ns(FAB_NS, 'select').value rescue '/'))
+      @mode = (xml.attributes.get_attributes_ns(FAB_NS, 'mode').value rescue 'overwrite')
       @arcs = [ ]
       xml.each_element do |e|
         if e.namespaces.namespace.href == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' && e.name == 'RDF'
@@ -16,6 +17,7 @@ module Fabulator
     end
 
     def run(context)
+      Rails.logger.info("Running an assertion")
       return true if self.rdf_model.nil?
       data = @from.run(context.data)
       if data.is_a?(Array)
@@ -35,6 +37,7 @@ module Fabulator
   protected
 
     def execute_arcs(data)
+      Rails.logger.info("Running: #{YAML::dump(@arcs)}")
       @arcs.each do |arc_set|
         # we want to make sure all of the arcs are in the database
         # arcs have well-defined nodes (not blank nodes or <rdf:li/> predicates
@@ -65,37 +68,70 @@ module Fabulator
       return true
     end
 
+    def add_triple2(s,p,o)
+      Rails.logger.info("add_triple2(#{s}, #{o}, #{p})")
+      Rails.logger.info("  mode: #{@mode}")
+      return if self.rdf_model.count_statements(s,p,o) > 0
+      if @mode == 'overwrite' && !o.bnode?
+        Rails.logger.info("Removing statements first")
+        self.rdf_model.find_statements(s,p,nil).each do |st|
+          self.rdf_model.remove_statement(st.subject, st.predicate, st.object)
+        end
+      end
+      self.rdf_model.add_statement(s,p,o)
+    end
+
     def add_triple(data,bnodes,s,t)
-      case t[0]
+      Rails.logger.info("Adding triple (#{s}, #{t[0]}, #{t[1]}, #{t[2]})")
+      t0 = t[0]
+      if t0 & 16 && s =~ /^\{(.*)\}$/
+        t0 = t0 - 16
+Rails.logger.info("Data: #{YAML::dump(data)}")
+        s = data.eval_expression($1).first.value
+      end
+      if t0 & 32 && t[1] =~ /^\{(.*)\}$/
+        t0 = t0 - 32
+        p = data.eval_expression($1).first.value
+      else
+        p = t[1]
+      end
+      if t0 & 64 && t[2] =~ /^\{(.*)\}$/
+        t0 = t0 - 64
+        o = data.eval_expression($1).first.value
+      else
+        o = t[2]
+      end
+        
+      case t0
         when 0:
-          rdf_model.add_statement(
-            RdfResource.from_uri(s),
-            RdfResource.from_uri(t[1]),
-            self.resource_from_data(data,t[2])
+          self.add_triple2(
+            RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+            RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
+            self.resource_from_data(data,o)
           )
         when 1:
           if s =~ /^\?_(.*)/
             v = $1
-            self.rdf_model.add_statement(
-              RdfResource.from_uri(bnodes[v]),
-              RdfResource.from_uri(t[1]),
-              self.resource_from_data(data,t[2])
+            self.add_triple2(
+              RdfResource.from_uri(bnodes[v], self.rdf_model.rdf_namespace),
+              RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
+              self.resource_from_data(data,o)
             )
           else
-            self.rdf_model.add_statement(
+            self.add_triple2(
               self.resource_from_data(data,s),
-              self.resource_from_data(data,t[1]),
-              self.resource_from_data(data,t[2])
+              self.resource_from_data(data,p),
+              self.resource_from_data(data,o)
             )
           end
         when 2:
           # predicate is variable
           if t[1] =~ /^\?_(.*)/ # blank node predicate (Bag/Seq)
             possible = rdf_model.find_statements(
-              RdfResource.from_uri(s),
+              RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
               nil,
-              self.resource_from_data(data,t[2])
-            ).select{|s| s.predicate.rdf_namespace.namespace == RDFS_NS}
+              self.resource_from_data(data,o)
+            ).select{|s| s.predicate.rdf_namespace.rdf_namespace == RDFS_NS}
             if possible.empty?
               # instantiate
             
@@ -106,10 +142,10 @@ module Fabulator
             v = $1
             return if data.get_values(v).first.nil?
             # variable predicate - we need to get from data
-            self.rdf_model.add_statement(
-              RdfResource.from_uri(s),
-              self.resource_from_data(data,t[1]),
-              self.resource_from_data(data,t[2])
+            self.add_triple2(
+              RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+              self.resource_from_data(data,p),
+              self.resource_from_data(data,o)
             )
           end
         when 3:
@@ -117,45 +153,45 @@ module Fabulator
           if s =~ /^\?_(.*)/
             v = $1
             return if bnodes[v].nil?
-            self.add_triple(data,bnodes,bnodes[v],[2, t[1], t[2]])
+            self.add_triple(data,bnodes,bnodes[v],[2, p, o])
           elsif s =~ /^\?(.*)/
             v = $1
             data_v = data.get_values(v).first
             return if data_v.nil?
-            self.add_triple(data,bnodes,data_v,[2, t[1], t[2]])
+            self.add_triple(data,bnodes,data_v,[2, p, o])
           end
         when 4:
           # object is variable
           if t[2].is_a?(Array)
-            self.rdf_model.add_statement(
-              RdfResource.from_uri(s),
-              RdfResource.from_uri(t[1]),
-              self.resource_from_data(data,t[2])
+            self.add_triple2(
+              RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+              RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
+              self.resource_from_data(data,o)
             )
           elsif t[2] =~ /^\?_(.*)/
             v = $1
             if bnodes[v].nil?
               possible = self.rdf_model.find_statements(
-                RdfResource.from_uri(s),
-                RdfResource.from_uri(t[1]),
+                RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+                RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
                 nil
               ).select {|n| n.bnode? }
               if possible.empty?
                 b = RdfResource.create_bnode
                 bnodes[v] = b.uri
-                self.rdf_model.add_statement(
-                  RdfResource.from_uri(s),
-                  RdfResource.from_uri(t[1]),
+                self.add_triple2(
+                  RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+                  RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
                   b
                 )
               else
                 bnodes[v] = possible.first.uri
               end
             else
-              self.rdf_model.add_statement(
-                RdfResource.from_uri(s),
-                RdfResource.from_uri(t[1]),
-                RdfResource.from_uri(bnodes[v])
+              self.add_triple2(
+                RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+                RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
+                RdfResource.from_uri(bnodes[v], self.rdf_model.rdf_namespace)
               )
             end
           else
@@ -163,24 +199,24 @@ module Fabulator
             v = $1
             if data.get_values(v).first.nil?
               possible = self.rdf_model.find_statements(
-                RdfResource.from_uri(s),
-                RdfResource.from_uri(t[1]),
+                RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+                RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
                 nil
               ).select{ |n| !n.bnode? && !n.literal? }
               if possible.empty?
-                self.rdf_model.add_statement(
-                  RdfResource.from_uri(s),
-                  RdfResource.from_uri(t[1]),
-                  self.resource_from_data(data,t[2])
+                self.add_triple2(
+                  RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+                  RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
+                  self.resource_from_data(data,o)
                 )
               else
                 data.create_child(v, possible.first.uri)
               end
             else
-              self.rdf_model.add_statement(
-                RdfResource.from_uri(s),
-                RdfResource.from_uri(t[1]),
-                self.resource_from_data(data,t[2])
+              self.add_triple2(
+                RdfResource.from_uri(s, self.rdf_model.rdf_namespace),
+                RdfResource.from_uri(p, self.rdf_model.rdf_namespace),
+                self.resource_from_data(data,o)
               )
             end
           end
@@ -189,7 +225,7 @@ module Fabulator
           if s =~ /^\?_(.*)/ # bnode
             v = $1
             return if bnodes[v].nil?
-            self.add_triple(data,bnodes,bnodes[v],[4, t[1], t[2]])
+            self.add_triple(data,bnodes,bnodes[v],[4, p, o])
           else 
             s =~ /^\?(.*)/
             v = $1
@@ -197,9 +233,9 @@ module Fabulator
             if data_v.nil?
               ss = self.rdf_model.create_resource
               data.create_child(v,ss.uri)
-              self.add_triple(data,bnodes,ss.uri,[4, t[1], t[2]])
+              self.add_triple(data,bnodes,ss.uri,[4, p, o])
             else
-              self.add_triple(data,bnodes,data_v,[4, t[1], t[2]])
+              self.add_triple(data,bnodes,data_v,[4, p, o])
             end
           end
         when 6:
@@ -234,10 +270,10 @@ module Fabulator
           data.create_child(v, r.uri)
           return r
         else
-          return RdfResource.from_uri(data_v)
+          return RdfResource.from_uri(data_v, self.rdf_model.rdf_namespace)
         end
       else
-        return RdfResource.from_uri(r)
+        return RdfResource.from_uri(r, self.rdf_model.rdf_namespace)
       end
     end
   end
