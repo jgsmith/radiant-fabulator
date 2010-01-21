dc8f0dcef13b2d090684fd6a0109bee070e21f22
James Smith <jgsmith@tamu.edu>
2010-01-21 22:38:11 +0000

    Pass data merging, etc., off to the XSM context object

--- lib/fabulator/context.rb
+++ lib/fabulator/context.rb
@@ -15,47 +15,11 @@ module Fabulator
     def merge!(d, path=nil)
       return if @data.nil?
       return @data.merge_data(d,path)
-      
-      bits = [] + path
-      last_bit = bits.pop
-      c = @data
-      bits.each do |b|
-        if c.is_a?(Array)
-          c[b.to_i] = { } if c[b.to_i].nil?
-          c = c[b.to_i]
-        else
-          c[b] = { } if c[b].nil?
-          c = c[b]
-        end
-      end
-      if c[last_bit].is_a?(Array) 
-        if d.is_a?(Array)
-          c[last_bit] = c[last_bit] + d
-        else
-          c[last_bit] << d
-        end
-      else
-        c[last_bit] = d
-      end
     end
 
     def clear(path = nil)
       return if @data.nil?
       return @data.clear(path)
-      
-      bits = [] + path
-      last_bit = bits.pop
-      c = @data
-      bits.each do |b|
-        if c.is_a?(Array)
-          c[b.to_i] = { } if c[b.to_i].nil?
-          c = c[b.to_i]
-        else
-          c[b] = { } if c[b].nil?
-          c = c[b]
-        end
-      end
-      c[last_bit] = { }
     end
 
     def context
@@ -70,19 +34,6 @@ module Fabulator
     def get(p = nil)
       return if @data.nil?
       return @data.get(p)
-      bits = [ ] +  p
-      last_bit = bits.pop
-      c = @data
-      bits.each do |b|
-        if c.is_a?(Array)
-          c[b.to_i] = { } if c[b.to_i].nil?
-          c = c[b.to_i]
-        else
-          c[b] = { } if c[b].nil?
-          c = c[b]
-        end
-      end
-      c[last_bit]
     end
   end
 end
